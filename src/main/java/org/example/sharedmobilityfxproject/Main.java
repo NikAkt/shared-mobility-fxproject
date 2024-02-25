732a959d398b8c0ae3931b1eab0404f875c0bf87
TugrulGit <business.tugrul.yilmaz@gmail.com>
2024-02-25 14:21:56 +0000

    Added root node, scene and a stage

--- src/main/java/org/example/sharedmobilityfxproject/Main.java
+++ src/main/java/org/example/sharedmobilityfxproject/Main.java
@@ -2,10 +2,13 @@ package org.example.sharedmobilityfxproject;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.example.sharedmobilityfxproject.model.Map;
 import org.example.sharedmobilityfxproject.model.Player;
 
-public class Main {
+public class Main extends Application{
     public static void main(String[] args) {
         // Create a Player object
         Player player = new Player(0, 0);
@@ -19,5 +22,16 @@ public class Main {
 
         // Print new player position
         System.out.println("New player position: (" + Player.getCoordX() + ", " + Player.getCoordY() + ")");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+//        Below is for creating another stage named stageName
+//        Stage stageName = new Stage();
+//        Most basic node type is Group type
+        Group root = new Group(); // create the node
+        Scene scene = new Scene(root); // pass the node to the scene
+        stage.setScene(scene); // pass the scene to the stage
+        stage.show(); // stage > scene > node
     }
 }
