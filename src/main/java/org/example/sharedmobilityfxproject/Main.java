f6da9a1124dc83daf1b85dd6d4a44277bce6823e
TugrulGit <business.tugrul.yilmaz@gmail.com>
2024-04-09 18:06:13 +0100

    fix: add previous initialization codes

--- src/main/java/org/example/sharedmobilityfxproject/Main.java
+++ src/main/java/org/example/sharedmobilityfxproject/Main.java
@@ -4,58 +4,32 @@ import javafx.application.Application;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import org.example.sharedmobilityfxproject.controller.GameController;
+import org.example.sharedmobilityfxproject.controller.SceneController;
+
 // Main class extends Application for JavaFX application
 public class Main extends Application {
 
-
-
-
-    // GYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSS
-//    public SceneController sceneController;
-//    public GameController gameController;
-//    GYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSS
+    public SceneController sceneController;
+    public GameController gameController;
 
     // Label to keep track of total carbon footprint
     Label carbonFootprintLabel; // Label to display carbon footprint
 
-    // Player (will be implemented)
-//    private Point player;
-
-
-
-//    removed from scene
-//    ImageView imageView = new ImageView( new Image( "https://upload.wikimedia.org/wikipedia/commons/c/c7/Pink_Cat_2.jpg"));
-
-
-
-//    GYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSS
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//@Override
-//public void start(Stage primaryStage) throws Exception {
-//    try {
-//        gameController = new GameController();
-//        gameController.startGame(primaryStage);
-//
-//    } catch (Exception e) {
-//        e.printStackTrace();
-//    }
-//}
-//
-//}
-//    GYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSSGYUWONSSSSSS
-
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
     public static void main(String[] args) {
         launch(args);
     }
+@Override
+public void start(Stage primaryStage) throws Exception {
+    try {
+        gameController = new GameController();
+        gameController.startGame(primaryStage);
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+
+}
+
 
