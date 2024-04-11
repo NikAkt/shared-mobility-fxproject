173a859ef49f4c6ebe54374c15a7724cc0c6852e
TugrulGit <business.tugrul.yilmaz@gmail.com>
2024-04-11 21:09:49 +0100

    feat: define colors and modify function

--- src/main/java/org/example/sharedmobilityfxproject/model/Map.java
+++ src/main/java/org/example/sharedmobilityfxproject/model/Map.java
@@ -3,6 +3,7 @@ package org.example.sharedmobilityfxproject.model;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
+import java.awt.Color;
 
 public class Map {
 
@@ -12,39 +13,57 @@ public class Map {
     public Map() {
     }
 
-    public static void main(String[] args) throws Exception {
+    public int[][] loadMap() throws Exception {
         // Load the image
-        File imageFile = new File("path_to_your_image.png");
+        File imageFile = new File("src/main/resources/images/Manhattan.png");
         BufferedImage image = ImageIO.read(imageFile);
 
         // The grid size
-        int rows = 80;
-        int columns = 120;
-        int[][] mapArray = new int[rows][columns];
+        final int ROWS = 80;
+        final int COLUMNS = 120;
+        int[][] mapArray = new int[ROWS][COLUMNS];
 
         // Dimensions of each cell
-        int cellWidth = image.getWidth() / columns;
-        int cellHeight = image.getHeight() / rows;
+        int cellWidth = image.getWidth() / COLUMNS;
+        int cellHeight = image.getHeight() / ROWS;
+
+        // Define colors
+        Color YELLOW_BUSSTOP = Color.decode("#ffeb3b");
+        Color MAROON_METRO = Color.decode("#990030");
+        Color GRAY_OBSTACLE = Color.decode("#808080");
+        Color BLUE_LAKE = Color.decode("#0000ff");
+        Color GREEN_GRASS = Color.decode("#00ff00");
+        Color WHITE_ROAD = Color.decode("#FFFFFF");
+        Color BLACK_BLOCKAGE = Color.decode("#000000");
 
         // Process each cell
-        for (int row = 0; row < rows; row++) {
-            for (int column = 0; column < columns; column++) {
+        for (int row = 0; row < ROWS; row++) {
+            for (int column = 0; column < COLUMNS; column++) {
                 // Get the color of the center pixel in the current cell
-                int pixel = image.getRGB(column * cellWidth + cellWidth / 2, row * cellHeight + cellHeight / 2);
-                int red = (pixel >> 16) & 0xff;
-                int green = (pixel >> 8) & 0xff;
-                int blue = (pixel) & 0xff;
+                Color pixelColor = new Color(image.getRGB(column * cellWidth + cellWidth / 2, row * cellHeight + cellHeight / 2));
 
                 // Assign values based on color
-                if (red == 255 && green == 255 && blue == 255) {
-                    // White
-                    mapArray[row][column] = 0;
-                } else if (red == 0 && green == 255 && blue == 0) {
-                    // Green
-                    mapArray[row][column] = 1;
-                } else if (red == 128 && green == 128 && blue == 128) {
-                    // Grey
-                    mapArray[row][column] = 4;
+                if (pixelColor.equals(YELLOW_BUSSTOP)) {
+                    // Assign a special value when the pixel color is YELLOW_BUSSTOP
+                    mapArray[row][column] = /* Your special value here */;
+                }if (pixelColor.equals(MAROON_METRO)) {
+                    // Assign a special value when the pixel color is YELLOW_BUSSTOP
+                    mapArray[row][column] = /* Your special value here */;
+                }if (pixelColor.equals(GRAY_OBSTACLE)) {
+                    // Assign a special value when the pixel color is YELLOW_BUSSTOP
+                    mapArray[row][column] = /* Your special value here */;
+                }if (pixelColor.equals(BLUE_LAKE)) {
+                    // Assign a special value when the pixel color is YELLOW_BUSSTOP
+                    mapArray[row][column] = /* Your special value here */;
+                }if (pixelColor.equals(GREEN_GRASS)) {
+                    // Assign a special value when the pixel color is YELLOW_BUSSTOP
+                    mapArray[row][column] = /* Your special value here */;
+                }if (pixelColor.equals(WHITE_ROAD)) {
+                    // Assign a special value when the pixel color is YELLOW_BUSSTOP
+                    mapArray[row][column] = /* Your special value here */;
+                }if (pixelColor.equals(BLACK_BLOCKAGE)) {
+                    // Assign a special value when the pixel color is YELLOW_BUSSTOP
+                    mapArray[row][column] = /* Your special value here */;
                 } else {
                     // Default case, might need to adjust based on actual image
                     mapArray[row][column] = -1;
@@ -52,6 +71,6 @@ public class Map {
             }
         }
 
-        // Here you can now use mapArray as needed
+        return mapArray;
     }
 }
