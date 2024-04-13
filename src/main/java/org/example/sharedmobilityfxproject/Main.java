a9069d8698345f93d6129e6e6669c2a9c4c6bbe9
TugrulGit <business.tugrul.yilmaz@gmail.com>
2024-04-13 17:09:00 +0100

    feat: initialize the MainController in Main.java

--- src/main/java/org/example/sharedmobilityfxproject/Main.java
+++ src/main/java/org/example/sharedmobilityfxproject/Main.java
@@ -1,40 +1,15 @@
 package org.example.sharedmobilityfxproject;
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.PauseTransition;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
-import javafx.util.Duration;
-import org.example.sharedmobilityfxproject.controller.GameController;
-import org.example.sharedmobilityfxproject.controller.KeyboardActionController;
+import org.example.sharedmobilityfxproject.controller.MainController;
 import org.example.sharedmobilityfxproject.controller.SceneController;
-import org.example.sharedmobilityfxproject.model.*;
-import org.example.sharedmobilityfxproject.model.tranportMode.Bus;
-import org.example.sharedmobilityfxproject.model.tranportMode.Taxi;
 import org.example.sharedmobilityfxproject.view.GameView;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 // Main class extends Application for JavaFX application
 public class Main extends Application {
-
-    public SceneController sceneController;
-    public GameController gameController;
 
     // Label to keep track of total carbon footprint
     Label carbonFootprintLabel; // Label to display carbon footprint
@@ -45,6 +20,7 @@ public class Main extends Application {
         try {
             GameView gameView = new GameView(primaryStage);
             SceneController sceneController = new SceneController(gameView);
+            MainController mainController = new MainController(sceneController, gameView);
 
         } catch (Exception e) {
             e.printStackTrace();
