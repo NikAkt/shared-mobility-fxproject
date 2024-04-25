a5f2906c9ec99e877839639ad639a482792b2411
TugrulGit <business.tugrul.yilmaz@gmail.com>
2024-04-25 14:38:54 +0100

    fix: uncomment scale

--- src/main/java/org/example/sharedmobilityfxproject/view/GameView.java
+++ src/main/java/org/example/sharedmobilityfxproject/view/GameView.java
@@ -314,17 +314,17 @@ public class GameView {
         StackPane root = new StackPane();
         scene = new Scene(root, WIDTH, HEIGHT);
 
-//        if (scale == null) {
-//            scale = new Scale(3, 3);  // Set the initial scale TODO: scale changed here
-//        } else {
-//            scale.setX(3);
-//            scale.setY(3);
-//        }
-//
-//        boolean scaleExists = grid.getTransforms().stream().anyMatch(transform -> transform instanceof Scale);
-//        if (!scaleExists) {
-//            grid.getTransforms().add(scale);  // Only add scale if not already present
-//        }
+        if (scale == null) {
+            scale = new Scale(3, 3);  // Set the initial scale TODO: scale changed here
+        } else {
+            scale.setX(3);
+            scale.setY(3);
+        }
+
+        boolean scaleExists = grid.getTransforms().stream().anyMatch(transform -> transform instanceof Scale);
+        if (!scaleExists) {
+            grid.getTransforms().add(scale);  // Only add scale if not already present
+        }
 
         // Game Lable Setting
 
